ff7ef8c17c3e38f24ec2d4419f38ccbe5ac3eab7
butters88 <butters1@gmail.com>
2015-06-13 18:38:47 +1000

    cluster config for node

--- app.js
+++ app.js
@@ -3,25 +3,34 @@
  */
 
 var express = require('express')
-
+var cluster = require('cluster')
 var app = express()
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
+if (cluster.isMaster) {
+	var cpuCount = require('os').cpus().length;
 
-app.use(express.static(__dirname + '/public'))
+	for (var i = 0; i < cpuCount; i++) {
+		cluster.fork();
+	}
+}
+else {
+    app.set('views', __dirname + '/views')
+	app.set('view engine', 'jade')
+	app.use(express.logger('dev'))
 
-app.get('/', function (req, res) {
-	res.render('index',
-		{ title : 'Home'}
-	)
-})
+	app.use(express.static(__dirname + '/public'))
 
-app.get('/beer', function (req, res) {
-	res.render('beer',
-		{ title : 'Beer'}
-	)
-})
+	app.get('/', function (req, res) {
+		res.render('index',
+			{ title : 'Home'}
+		)
+	})
 
-app.listen(3000)
+	app.get('/beer', function (req, res) {
+		res.render('beer',
+			{ title : 'Beer'}
+		)
+	})
+
+	app.listen(3000)
+}
