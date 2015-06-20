35b235f3100c8f118b0e95ca8fcc5ec00561e4ef
butters88 <butters1@gmail.com>
2015-06-20 11:48:22 +1000

    express-generator output

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-/* 
- * Module dependencies
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var express = require('express')
-var cluster = require('cluster')
-var app = express()
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-if (cluster.isMaster) {
-	var cpuCount = require('os').cpus().length;
+var app = express();
 
-	for (var i = 0; i < cpuCount - 1; i++) {
-		cluster.fork();
-	}
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
-else {
-    app.set('views', __dirname + '/views')
-	app.set('view engine', 'jade')
-	app.use(express.logger('dev'))
 
-	app.use(express.static(__dirname + '/public'))
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-	app.get('/', function (req, res) {
-		res.render('index',
-			{ title : 'Home'}
-		)
-	})
 
-	app.get('/beer', function (req, res) {
-		res.render('beer',
-			{ title : 'Beer'}
-		)
-	})
-
-	app.get('/pong', function (req, res) {
-		res.render('pong',
-			{ title : 'Ping Pong'}
-		)
-	})
-
-	app.listen(3000)
-}
+module.exports = app;
