b5a6dbda9472dda00fc3b2c14e84c906efe0aa76
butters88 <butters1@gmail.com>
2015-08-12 17:29:31 +1000

    added routes for beer and pong pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to The Bellthorpe' });
+});
+
+router.get('/beer', function(req, res, next) {
+  res.render('beer', { title: 'The Bellthorpe - Beer'});
+});
+
+router.get('/pong', function(req, res, next) {
+  res.render('pong', { title: 'The Bellthorpe - Pong'});
 });
 
 module.exports = router;
